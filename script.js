29519d05f42126c9d2e111603149c17e923e00e9
Fiza Khan <fiza1892003@gmail.com>
2023-08-04 18:28:43 +0530

    changes

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+function customerFunction(){
+    window.location.href = 'customer.html';
+};
+
 var map = L.map('map').setView([51.505, -0.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
